1b2cbec560be64eefe21f9c75f460220b84844de
yanrivera19 <yan.rivera.6@gmail.com>
2021-11-27 22:25:12 -0800

    Finished code. Pending revisal. Worked on UI styling.

--- script.js
+++ script.js
@@ -6,39 +6,34 @@ const ingredients = document.getElementById("ingrList");
 const instructions = document.getElementById("instructions");
 const newRecipeBtn = document.getElementById("newRecipeBtn");
 
-
+const mealImg = document.createElement("img");
+mealImg.className = " meal-img";
+mealImg.style.display = "none";
 
 function displayRecipe(recipe) {
 	console.log(recipe);
-	
-	// const textNodeMealName = document.createTextNode(recipe.strMeal);
+
 	mealName.innerHTML = recipe.strMeal;
 
-	const mealImg = document.createElement("img");
+	mealImg.style.display = "block";
 	mealImg.src = recipe.strMealThumb;
-	mealImg.className = "meal-img"
 	images.appendChild(mealImg);
+	
 	const recipeVid = recipe.strYoutube;
-
 	videoGuide.innerHTML = `Click on the button to watch how to make ${recipe.strMeal}:`
 	videoBtn.addEventListener("click", function() {window.open(recipeVid, 'new_window')});
-
 	
 	let arrayOfIngredients = [];
-	let arrayOfMeasures = [];
-	for(let i = 0; i < 20; i++) {
+	for(let i = 0; i < 20; i++) {		
 		console.log(recipe.strIngredient1);
 		const individualIngredients = recipe[`strIngredient${i}`];
 		const individualMeasures = recipe[`strMeasure${i}`];
-		if(individualIngredients && individualMeasures) {
-			arrayOfIngredients.push(individualIngredients);
-			arrayOfMeasures.push(individualMeasures.toUpperCase());
-			const ingredientsList = document.createElement("li");
-			ingredients.appendChild(ingredientsList);
-			const ingredientsTextNode = document.createTextNode(individualMeasures + " " + individualIngredients)
-			ingredientsList.appendChild(ingredientsTextNode)
-		}
+		if(individualIngredients) {
+			arrayOfIngredients.push(individualMeasures + " " + individualIngredients);
+			console.log(arrayOfIngredients);
+		};
 	}
+	ingredients.innerHTML = '<li>' + arrayOfIngredients.join("</li><li>"); + '</li>';
 
 	instructions.innerHTML = recipe.strInstructions;
 };
@@ -50,11 +45,27 @@ function getRecipes() {
 	        const recipe = response.data;
 	        console.log(`GET list recipe`, recipe);
 	        displayRecipe(recipe.meals[0]);
+
         })
         .catch(error => console.error(error));
 };
 
 getRecipes();
+
+function getNewRecipes() {
+	axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+	    .then(response => {
+	        console.log(response);
+	        const recipe = response.data;
+	        console.log(`GET list recipe`, recipe);
+	        mealImg.remove();
+	        ingredients.innerHTML = "";
+	        displayRecipe(recipe.meals[0]);	      
+        })
+          
+        .catch(error => console.error(error));
+};
+
 
 
 
